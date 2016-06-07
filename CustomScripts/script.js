c94122f8042178832444c4cc9a5c795ad0552eab
Srikanth Goud <srikanthp.1453@hotmail.com>
2016-06-07 17:43:41 +0530

    Update script.js

--- CustomScripts/script.js
+++ CustomScripts/script.js
@@ -59,9 +59,16 @@ MyCvApp.controller('resumeController', function ($scope) {
 
 MyCvApp.controller('contactController', function ($scope) {
 
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-});
+  $scope.map = { center: { latitude: 17.355574942128815,  longitude: 78.5382288694381745 }, zoom: 12 ,
+markers: [{
+                   
+                    latitude: 17.355574942128815,
+                    longitude: 78.5382288694381745 ,
+                    title: 'Srikanth'
+                }]
 
+ };
+});
 MyCvApp.controller('projectsController', function ($scope) {
     $scope.message = 'sfdsf';
 });
